8df9ab6c221690b2e558ad9565dbd699036ce635
SHYAM SUNDAR K N <shyam01.sundar24@gmail.com>
2025-08-08 12:58:23 +0530

    Python & Jenkins file updated successfully

---
+++ demoxy.py
@@ -0,0 +1,7 @@
+#First python test file
+
+print("This is a Jenkins build file !")
+print("##############################")
+print("Building Jenkins pipeline ok !!!")
+print("###############################")
+print("Thank You !!")
